3f5d690b5e0d42673a8510d2b2167f600f26de32
Geilson <geilson@iMac-de-Ferguss.local>
2017-02-05 20:20:23 -0200

    Initial Commit

---
+++ Curso-c-UFMG/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Curso-c-UFMG
+//
+//  Created by Geilson on 05/02/17.
+//  Copyright © 2017 Fergus Sistemas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
